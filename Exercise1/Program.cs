7165507e93b7a973945106fe676d65fb9c8298c7
Viktor Näslund <1299835+Raiu@users.noreply.github.com>
2024-05-21 10:16:38 +0200

    Move input validation from Parse. Will also resolve warnings from compiler.

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -6,19 +6,21 @@ namespace Exercise1
 
     /***
      * 1)   Classes that should be included for this problem: Program, RecodBook, Employee.
-     * 2)   Program should contain a run loop, parseinputs and validate input and handle the main logic.
+     * 2)   Program should contain a run for the main logic, input parsing and validation, logic for the different commands
      *      RecordBook should contain said record and be able to add and remove employes from said recod in either list or dict
      *      Employee should contain properties for handling Firstname, Surname and salaries and some validation.
      */
 
     class Employee
     {
+        public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public double Salary { get; set; }
 
         public Employee(string firstName, string lastName, double salary)
         {
+            ID = new Random().Next(int.MinValue, int.MaxValue);
             FirstName = firstName;
             LastName = lastName;
             Salary = salary;
@@ -39,18 +41,22 @@ namespace Exercise1
             Employee employee = new Employee(firstName, lastName, Salary);
             Registry.Add(employee);
         }
+
+        public void RemoveEmployee(string firstName, string lastName)
+        {
+
+        }
     }
 
 
     class Program
     {
-        private EmployeeRegistry Book;
+        private EmployeeRegistry Book {  get; set; } = new EmployeeRegistry();
         private readonly CultureInfo Culture = new CultureInfo("sv-SE");
 
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.Book = new();
             program.run();
         }
 
@@ -61,15 +67,18 @@ namespace Exercise1
             bool running = true;
             while (running)
             {
-                (string command, string[] arguments) = ParseInput(Console.ReadLine());
+                string? input = Console.ReadLine();
 
-                if (command == "")
+                if (string.IsNullOrWhiteSpace(input))
                 {
-                    Console.WriteLine("Invalid input");
+                    Console.WriteLine("Error: Invalid input");
+                    Console.WriteLine("Enter your command:");
                     continue;
                 }
 
-                switch (command)
+                (string command, string[] arguments) = ParseInput(input);
+
+                switch (command.ToLower())
                 {
                     case "quit":
                         running = false;
@@ -93,11 +102,6 @@ namespace Exercise1
 
         private (string, string[]) ParseInput(string input)
         {
-            if (string.IsNullOrWhiteSpace(input))
-            {
-                return ("", Array.Empty<string>());
-            }
-
             string[] segments = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
             string command = segments[0];
             string[] arguments = segments.Skip(1).ToArray();
@@ -119,7 +123,8 @@ namespace Exercise1
         private void AddEmployee(string[] employeeData)
         {
             if (employeeData.Length != 3) {
-                Console.WriteLine($"{String.Join(' ', employeeData)} is not a valid input string for the add command");
+                Console.WriteLine($"{String.Join(' ', employeeData)} is not a valid input");
+                Console.WriteLine("Usage:   add <first name> <last name> <salary>");
                 return;
             }
 
@@ -127,7 +132,7 @@ namespace Exercise1
             string lastName = employeeData[1];
             if (!double.TryParse(employeeData[2], Culture, out double salary))
             {
-                Console.WriteLine($"{employeeData[2]} is not valid salary value");
+                Console.WriteLine($"{employeeData[2]} is not a valid salary value");
                 return;
             }
 
